d136198ba9b684cf5de94aed91399a87092aff61
Sanjai babu s <bsanjay235@gmail.com>
2024-09-22 13:09:21 +0530

    set up the basics of projects

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import { Box } from "@mui/material";
+import React from "react";
+// import Button from '../components/Button';
+
+const Home = () => (
+  <Box>
+    <h1>hello react</h1>
+  </Box>
+);
+
+export default Home;
